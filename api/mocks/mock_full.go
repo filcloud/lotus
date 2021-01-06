958c4e9eb0e84c9f652e0bd90ceb197c8f5d72e9
Frank <wholery@163.com>
2021-01-06 15:38:11 +0800

    update api for jsonrpc

--- api/mocks/mock_full.go
+++ api/mocks/mock_full.go
@@ -2005,14 +2005,12 @@ func (mr *MockFullNodeMockRecorder) StateCall(arg0, arg1, arg2 interface{}) *gom
 }
 
 // StateChangedActors mocks base method
-func (m *MockFullNode) StateChangedActors(arg0 context.Context, arg1, arg2 cid.Cid) (map[string]types.Actor, map[string]types.Actor, map[string]types.Actor, error) {
+func (m *MockFullNode) StateChangedActors(arg0 context.Context, arg1, arg2 cid.Cid) (*api.ChangedActors, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "StateChangedActors", arg0, arg1, arg2)
-	ret0, _ := ret[0].(map[string]types.Actor)
-	ret1, _ := ret[1].(map[string]types.Actor)
-	ret2, _ := ret[2].(map[string]types.Actor)
-	ret3, _ := ret[3].(error)
-	return ret0, ret1, ret2, ret3
+	ret0, _ := ret[0].(*api.ChangedActors)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
 }
 
 // StateChangedActors indicates an expected call of StateChangedActors
